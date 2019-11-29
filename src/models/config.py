fixes css normalisation bug. tested and works for classification of trips

src/models/config.py
@@ -30,7 +30,7 @@ default_grids = {
     "LogisticRegression_cv":{
                 'C': [0.0001, 0.001, 0.01, 0.1, 1, 20, 50] + list(np.arange(2, 20, 1))
 
-                ,'fit_intercept': (True)
+                ,'fit_intercept': [True]
             },
     "SVM_cv":{"kernel":["poly"],"gamma":["auto","scale",0.1,1,10],"class_weight":["balanced",None],
                     'C':[0.1, 1, 0.5], "degree":[1,2],"coef0" :[0,0.1,1,0.001]},
@@ -86,32 +86,37 @@ space = {
             #
             # "resampler__kind": hp.choice("resampler__kind", ["borderline-1", "borderline-2"])
         }
-trips_dictionary = [{ # [M] Mandatory, [O] Optional
-    "Data": [{"features":"riverdf", "target":"wwfdf","target_col": "Trip","train_test_group":"Trip"}], # [M] What set of features and target data sets to use
+
+"""
+A sample dictionary used to run the water as labels experiments. We are using the dissimilarity case"""
+water_dictionary = { # [M] Mandatory, [O] Optional
+    "Data": [{"features":"riverdf", "target":"wwfdf","target_col": "Water","train_test_group":"Area_group"}], # [M] What set of features and target data sets to use
     # [O] The target_col is the column where the labels can be found, default "target"
     # [O] Train_test_group is the column used to split the set to train and
     # test, default "group" if it exists, if not then "target" column
-    "train_test_split_method": [{"name":"StratifiedKFold","n_splits":6}], # [O] How to generate train-test splits, default "StratifiedKFold
-    "models": [{# Inside here is information used for the training step of the procedure
-        "estimators": [{"name":"RandomForest","cv":"grid"}],# [M] Models to use for classification. Custom models
-        # can also be passed and custom grid spaces.
-        "resampler": [None],# [O] If not given, None is used
-        "css": [ "CSSLOG"], # [O] If not given, None is used
-        # [O] The hyperparameter search  If not given, Grid is used possible options are {Grid, Bayes, Random}
-        "scaler": [None], # [O] Scaler to use on data, default None
-        "validation":[{"name":"StratifiedKFold","n_splits":5}]
-        # [O] How to split train set to validation folds and which column of the meta data to use. Default value is
-        # the train_test splitting method and the train_test column.
-    },
+    "train_test_split_method": [{"name":"GroupKFold","n_splits":7}], # [O] How to generate train-test splits, default "StratifiedKFold
+    "models": [
         {
-            "estimators":[ {"name":"LogisticRegression","penalty":"l2","fit_intercept":True},
-                       {"name":"SVM","kernel":"poly","degree":1}],
-            "css": [ "CSSLOG"],
-            "scaler": [{"name":"StandardScaler","with_mean":False}],
-            "validation":[{"name":"StratifiedKFold","n_splits":5}]
-        }]
-}]
+            "estimators":[ {"name":"MultinomialNB"},{"name": "RandomForest"}],
+            "css": [ "CSSLOG","CSS",None],
+            "scaler": [None],
+            "resampler":[None,{"name":"RandomOverSampler","random_state":11235}],
+            "validation":[{"name":"GroupKFold","group_col":"Area_group","n_splits":6}]
+        },
+        {
+            "estimators": [{"name":"SVM","kernel":"poly","degree":1}],
+            "css": ["CSSLOG", "CSS", None],
 
+            "scaler": [{"name":"StandardScaler","with_mean":False}],
+            "resampler":[None,{"name":"RandomOverSampler","random_state":11235}],
+            "validation":[{"name":"GroupKFold","group_col":"Area_group","n_splits":6}]
+        }
+    ]
+}
+"""
+The program will loop through all dictionaries in the hypothesis list and execute them
+"""
+hypothesis = [water_dictionary]
 experiment_dictionary = [{ # [M] Mandatory, [O] Optional
     "Data": [{"features":"riverdf", "target":"wwfdf","target_col": "Trip","train_test_group":"Trip"}], # [M] What set of features and target data sets to use
     # [O] The target_col is the column where the labels can be found, default "target"

src/models/cssnormaliser.py
@@ -2,8 +2,9 @@ from rpy2.rinterface_lib.embedded import RRuntimeError
 import numpy as np
 from sklearn.base import TransformerMixin
 from rpy2.robjects.packages import importr
-
-
+from pandas import DataFrame
+import rpy2.robjects.numpy2ri
+rpy2.robjects.numpy2ri.activate()
 msq = importr("metagenomeSeq")
 # BiocManager = importr_tryhard("BiocManager","http://cran.us.r-project.org")
 # msq =importr_tryhard("metagenomeSeq","https://cran.ma.imperial.ac.uk/")
@@ -22,21 +23,30 @@ class CSSNormaliser(TransformerMixin):
         pass
 
     def fit(self, X, y=None):
+        if type(X) is type(DataFrame()):
+            X = X.to_numpy()
+
         MRTrain =msq.newMRexperiment(X.T)
         self.p = msq.cumNormStat(MRTrain)
         return(self)
     def transform(self, X):
         if self.identity:
             return X
-        MRTest = msq.newMRexperiment(X.T)
+        if type(X) is type(DataFrame()):
+            array = X.to_numpy()
+        else:
+            array = X
+        MRTest = msq.newMRexperiment(array.T)
         Normalisation =msq.cumNorm(MRTest, p = self.p)
         Rmatrix=msq.MRcounts(Normalisation,norm = True,log = self.log)
         # converting r matrix to python
         Normalised_numpy = np.array(Rmatrix).T
+        if type(X) is type(DataFrame()):
+            Normalised_numpy = DataFrame(Normalised_numpy,index = X.index,columns=X.columns)
         return(Normalised_numpy)
 
     def __hash__(self):
         return hash((self.log,self.identity))
 
-    def __str__(self):
+    def __repr__(self):
         return "CSSNormaliser(log = {},identity = {})".format(self.log,self.identity)

src/models/experiment.py
@@ -8,6 +8,10 @@ from sklearn.model_selection import StratifiedKFold
 from sklearn.preprocessing import FunctionTransformer
 from imblearn import FunctionSampler
 from sklearn.exceptions import NotFittedError
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning)
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+warnings.filterwarnings("ignore", category=FutureWarning)
 # import scipy.stats as st
 #
 # from sklearn.svm import SVC
@@ -218,6 +222,7 @@ class Experiment():
             # area number the new samples would take/
             set_for_kfold = ytrain
         # Scaling the train set
+        xtrain = self.css.fit_transform(xtrain)
         xtrain = self.scaler.fit_transform(xtrain)
 
         # Splitting the train set into validation folds that will be used in training
@@ -234,6 +239,7 @@ class Experiment():
 
         # The test sample has to be transformed to the same scale as the train set
         try:
+            xtest = self.css.transform(xtest)
             xtest = self.scaler.transform(xtest)
             return self.model.predict(xtest)
         except AttributeError as e:
@@ -242,24 +248,20 @@ class Experiment():
                   "and then Experiment.predict(test_features)")
             raise e
 
+    def return_dictionary(self):
+        """
+        returns some parameters of the instance in a dictionary
+        :return:
+        """
+        parameters = self.__dict__
+        parameters_to_print = ["estimator_name", "css", "scaler", "resampler", "names", "target_column",
+                               "cv", "train_test_split_method", "train_test_split_column", "validation_method",
+                               "validation_group", "estimator"]
+        self.parameters_dictionary = {i: parameters[i] for i in parameters_to_print}
+        return {i: parameters[i] for i in parameters_to_print}
+
     def __str__(self):
-        return(str(self.__dict__))
+        return(str(self.return_dictionary()))
 
 
-if __name__ == "__main__":
-    path_to_data = "../../data/processed/"
-    riverdf = pd.read_csv(path_to_data + "riverdf", index_col=0)
-    wwfdf = pd.read_csv(filepath_or_buffer=path_to_data + "wwfdf", encoding="ISO-8859-1", index_col="ID")
 
-    rfr_grid = {
-        'max_depth': (list(range(3, 7)) + [None]),
-        # 'n_estimators': [100, 300, 500, 1000],
-        # "min_samples_split": [2, 3, 4],
-        # "class_weight": ["balanced"],
-        "bootstrap": [False]
-    }
-    testExperiment = Experiment(model_name="RFR", css=CSSNormaliser(log=True), fold_generator=StratifiedKFold,
-                                x=riverdf, meta_data=wwfdf.Water, scaler=None, resampler=None,
-                                cv=mth.rfr_cv, grid=rfr_grid, penalty="l1")
-
-    testExperiment.run()